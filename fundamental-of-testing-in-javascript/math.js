f3baf75e063bb3e7c43337d069c742d674819ebd
tkmct <1220t.takamichi@gmail.com>
2018-12-09 02:40:22 +0900

    add async test

--- fundamental-of-testing-in-javascript/math.js
+++ fundamental-of-testing-in-javascript/math.js
@@ -1,4 +1,7 @@
 const sum = (a, b) => a - b
 const subtract = (a, b) => a - b
 
-module.exports = { sum, subtract }
+const sumAsync = (a, b) => Promise.resolve(sum(a, b))
+const subtractAsync = (a, b) => Promise.resolve(subtract(a, b))
+
+module.exports = { sum, subtract, sumAsync, subtractAsync }
