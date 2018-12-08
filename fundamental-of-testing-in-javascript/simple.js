23ee91ef5552f9abc81711d93e7ff4fa6f9a044a
tkmct <1220t.takamichi@gmail.com>
2018-12-08 23:51:36 +0900

    add expect

--- fundamental-of-testing-in-javascript/simple.js
+++ fundamental-of-testing-in-javascript/simple.js
@@ -2,14 +2,18 @@ const { sum, subtract } = require('./math')
 
 let result = sum(3, 7)
 let expected = 10
-
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
+expect(result).toBe(expected)
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    }
+  }
 }
