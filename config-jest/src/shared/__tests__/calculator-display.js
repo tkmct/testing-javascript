b7d1a0b2bde593354b6d85af1037fe7e610021aa
tkmct <1220t.takamichi@gmail.com>
2018-12-09 16:33:09 +0900

    Add emotion serializer

--- config-jest/src/shared/__tests__/calculator-display.js
+++ config-jest/src/shared/__tests__/calculator-display.js
@@ -1,9 +1,13 @@
 import 'react-testing-library/cleanup-after-each'
 import React from 'react'
 import {render} from 'react-testing-library'
+import {createSerializer} from 'jest-emotion'
+import * as emotion from 'emotion'
 import CalculatorDisplay from '../calculator-display'
 
-test('renders', () => {
+expect.addSnapshotSerializer(createSerializer(emotion))
+
+test('mounts', () => {
   const {container} = render(<CalculatorDisplay value="0" />)
   expect(container.firstChild).toMatchSnapshot()
 })
